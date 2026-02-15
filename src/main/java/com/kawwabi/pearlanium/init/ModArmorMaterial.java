6d66c67b0b668f4d54fd645dd8dd6c076f3d29c3
Kawwabi <viniciussms01@outlook.com>
2026-02-15 15:51:22 -0300

    armor sounds!!! The mod seems to be almost done now.

--- src/main/java/com/kawwabi/pearlanium/init/ModArmorMaterial.java
+++ src/main/java/com/kawwabi/pearlanium/init/ModArmorMaterial.java
@@ -9,13 +9,20 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public class ModArmorMaterial {
+    // Custom armor equip sound for Pearlanium
+    public static final SoundEvent PEARLANIUM_EQUIP_SOUND = SoundEvent.of(PearlaniumMod.id("item_armor_equip_pearlanium"));
+    public static final RegistryEntry<SoundEvent> PEARLANIUM_EQUIP = RegistryEntry.of(PEARLANIUM_EQUIP_SOUND);
+
+    // Custom armor equip sound for Wardium
+    public static final SoundEvent WARDIUM_EQUIP_SOUND = SoundEvent.of(PearlaniumMod.id("item_armor_equip_wardium"));
+    public static final RegistryEntry<SoundEvent> WARDIUM_EQUIP = RegistryEntry.of(WARDIUM_EQUIP_SOUND);
+
     public static final RegistryEntry<ArmorMaterial> PEARLANIUM = register("pearlanium", Map.of(
                     ArmorItem.Type.HELMET, 4,
                     ArmorItem.Type.CHESTPLATE, 9,
@@ -23,20 +30,20 @@ public class ModArmorMaterial {
                     ArmorItem.Type.BOOTS, 4
             ),
             22,
-            SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND,
+            PEARLANIUM_EQUIP,
             () -> Ingredient.ofItems(moditems.PEARLANIUM_INGOT),
             3.5F,
             0.1F,
             false);
 
-        public static final RegistryEntry<ArmorMaterial> WARDIUM = register("wardium", Map.of(
+    public static final RegistryEntry<ArmorMaterial> WARDIUM = register("wardium", Map.of(
                 ArmorItem.Type.HELMET, 5,
                 ArmorItem.Type.CHESTPLATE, 10,
                 ArmorItem.Type.LEGGINGS, 8,
                 ArmorItem.Type.BOOTS, 5
         ),
                 22,
-                SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND,
+                WARDIUM_EQUIP,
                 () -> Ingredient.ofItems(Items.ECHO_SHARD),
                 4,
                 0.2F,
